c0f233284180deae9c75f24ffb16dabf54fb8bcd
Michael <scentsome@gmail.com>
2020-07-03 15:34:23 +0800

    init commit

---
+++ app/src/main/java/com/demo/mygitapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.demo.mygitapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
