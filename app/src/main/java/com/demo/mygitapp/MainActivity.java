65b8af14c8c6ee70ecf67d34547db38914d9b2ef
joshuawang427 <joshuathesmart427@gmail.com>
2020-07-04 11:15:55 +0800

    My name is joshua

--- app/src/main/java/com/demo/mygitapp/MainActivity.java
+++ app/src/main/java/com/demo/mygitapp/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("OpenCV","Git Demo");
+        Log.d("OpenCV", "Git Demo");
 
     }
 
@@ -19,8 +19,13 @@ public class MainActivity extends AppCompatActivity {
         return a + b;
     }
 
-    int sub(int a, int b){
-        return a - b ;
+    int sub(int a, int b) {
+        return a - b;
     }
 
+    void Joshua(){
+        Log.d("omega", "I love miachel");
+    }
 }
+
+
